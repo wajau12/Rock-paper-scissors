c1bd2afae166a29c095fc816642970f708fd75a3
David Michael <micdavid1990@gmail.com>
2023-01-28 22:47:16 +0100

    added function to play single round of game

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ function getComputerChoice(){
     let choice = ["rock", "paper", "scissors"];
     
     let value = [Math.floor (Math.random() * choice.length)];
-    
+
     return choice[value];
 
 }
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+
+    let choice = ["rock", "paper", "scissors"];
+    playerSelection = playerSelection.toLowerCase();
+    
+}
