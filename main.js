0b8d9fc80968a27428836cf97d1d00d200293171
David Michael <micdavid1990@gmail.com>
2023-01-28 23:20:37 +0100

    Updated

--- main.js
+++ main.js
@@ -12,5 +12,22 @@ function playRound(playerSelection, computerSelection) {
 
     let choice = ["rock", "paper", "scissors"];
     playerSelection = playerSelection.toLowerCase();
+
+    if(!choice.includes(playerSelection, computerSelection)){
+        return (" " +  playerSelection + " is invalid Selection, please choose either rock, paper or scissors.");
+    }
+    else if(playerSelection === computerSelection){
+        return "Tie score";
+
+    } else if (playerSelection ==="rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock"){
+        return "You win! "  + playerSelection + " " + "beats " + computerSelection + ".";
+
+    } else {
+        return "You lose! "  + computerSelection + " " + "beats " + playerSelection + ".";
+    }
+    
     
 }
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
