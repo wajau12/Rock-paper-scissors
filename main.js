09b33f6a70afcacfd80bd89d49e78c9f9eb30ebd
David Michael <micdavid1990@gmail.com>
2023-01-28 01:06:24 +0100

    edit main.js and get computer choice

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function getComputerChoice(){
     let choice = ["rock", "paper", "scissors"];
     
     let value = [Math.floor (Math.random() * choice.length)];
+    
     return choice[value];
 
 }
